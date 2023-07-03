7b921f29a66320b4f496e916e6a912d7395940ca
hgoncal2 <aluno23327@ipt.pt>
2023-07-03 01:55:31 +0100

    com movimento

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 import subprocess
 import sys
 from threading import Thread
@@ -13,7 +13,7 @@ import json
 from multiprocessing.connection import Listener
 import socket
 import threading
-
+from markupsafe import Markup
 from turbo_flask import Turbo
 
 
@@ -30,17 +30,17 @@ val=""
 here = os.path.dirname(os.path.abspath(__file__))
 @app.context_processor
 def inject_load():
-	return {'load1':val}
+	return {'load1': val}
 
 def printit():
   	with app.app_context():
   		global val
   		val=""
   		inject_load()
-  		turbo.push(turbo.replace(render_template("index2.html"), 'load'))
+  		turbo.push(turbo.replace(render_template("alerts.html"), 'load'))
   		global t
   		t.cancel()
-  		t=threading.Timer(5.0, printit)
+  		t=threading.Timer(10.0, printit)
   		global flag_t
   		flag_t=1
   	
@@ -52,9 +52,10 @@ def back():
 	if request.args.get('q') == "move":
 		with app.app_context():
 			global val
-			val="DETECTADO MOVIMENTO!"
+			val='<div class="alert alert-warning alert-dismissible text-center " role="alert"><strong>Movimento Detectado!</strong></div>'
 			inject_load()
-			turbo.push(turbo.replace(render_template("index2.html"), 'load'))
+			print("entrei")
+			turbo.push(turbo.replace(render_template("alerts.html"), 'load'))
 			global flag_t
 			if(flag_t==1):
 				flag_t=0
@@ -77,8 +78,11 @@ def default():
 	val=""
 	return redirect(url_for('dashboard'))
 
-@app.route("/dashboard")
-def dashboard():
+
+
+@app.route("/dashboard/")
+@app.route("/dashboard/<path:path>")
+def dashboard(path=None):
 	global val
 	val=""
 	cnx = mysql.connector.connect(user='root', password='hugo2023',
@@ -125,12 +129,102 @@ def dashboard():
 		lista_d_t.reverse()
 		lista_d_h.reverse()
 		lista_v_h.reverse()
-	return render_template("base.html",data_v_t=json.dumps(lista_v_t),data_d_t=lista_d_t,data_v_h=json.dumps(lista_v_h),data_d_h=lista_d_h,val=val)
+	return render_template("base.html",data_v_t=json.dumps(lista_v_t),data_d_t=lista_d_t,data_v_h=json.dumps(lista_v_h),data_d_h=lista_d_h,val=val,pag="dash")
 
 
 
+@app.route('/conf')
+def conf():
+	return render_template("base.html",pag="conf")
 
-    
+@app.route('/history/<sensor>')
+def history(sensor):
+
+	cnx = mysql.connector.connect(user='root', password='hugo2023',
+                              host='127.0.0.1',port='3306',
+                              database='iot')
+	
+	if(sensor=="temp"):
+		cursor=cnx.cursor() 
+		lista_v_t=[]
+		lista_d_t=[]
+		t="select data from temp "
+		t1="select valor from temp "
+		
+		flag=0
+
+		cursor.execute(t1)
+		records = cursor.fetchall()
+		for row in records:
+		     lista_v_t.append(row[0])
+		cursor.execute(t)
+		records = cursor.fetchall()
+		for row in records:
+		     lista_d_t.append((row[0]).split(" "))
+		items=[]
+		lista_v_t.reverse()
+		lista_d_t.reverse()
+		for i in range(len(lista_v_t)):
+		    an_it=dict(id=str(i),valor=str(lista_v_t[i]) + "°C",data=str(lista_d_t[i][0]),hora=str(lista_d_t[i][1]))
+		    items.append(an_it)
+		return render_template("base.html",pag="history",items=items)
+		
+
+
+
+	if(sensor=="humid"):
+		cursor=cnx.cursor() 
+		lista_v_h=[]
+		lista_d_h=[]
+
+		h1="select data from humid "
+		h="select valor from humid "
+		
+		cursor.execute(h)
+		records = cursor.fetchall()
+		for row in records:
+		     lista_v_h.append(row[0])
+		cursor.execute(h1)
+		records = cursor.fetchall()
+		for row in records:
+		     lista_d_h.append((row[0]).split(" "))
+		items=[]
+		lista_d_h.reverse()
+		lista_v_h.reverse()
+		for i in range(len(lista_v_h)):
+		    an_it=dict(id=str(i),valor=str(lista_v_h[i])+"%",data=str(lista_d_h[i][0]),hora=str(lista_d_h[i][1]))
+		    items.append(an_it)
+		return render_template("base.html",pag="history",items=items)
+
+	if(sensor=="pir"):
+		cursor=cnx.cursor()
+		lista_p=[]
+		p="select data from pir"
+		cursor.execute(p)
+		records = cursor.fetchall()
+		for row in records:
+			lista_p.append((row[0]).split(" "))
+		items=[]
+		lista_p.reverse()
+		for i in range(len(lista_p)):
+		    an_it=dict(id=str(i),valor="Ativado",data=str(lista_p[i][0]),hora=str(lista_p[i][1]))
+		    items.append(an_it)
+		return render_template("base.html",pag="history",items=items)
+	
+	
+
+	return render_template("404.html")
+		
+
+
+	
+
+	
+
+	
+	
+
+
 @app.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='favicon.ico')
